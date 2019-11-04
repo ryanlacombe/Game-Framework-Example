168b07cc38445cbaa490362af8a579049ea59158
Ryan LaCombe <ryanalacombe@gmail.com>
2019-11-04 11:04:42 -0600

    Even more Matrix Math

--- GameFramework/Matrix3.cs
+++ GameFramework/Matrix3.cs
@@ -22,6 +22,24 @@ namespace GameFramework
             this.m21 = m21; this.m22 = m22; this.m23 = m23;
             this.m31 = m31; this.m32 = m32; this.m33 = m33;
         }
+        public Matrix3(Matrix3 matrix3)
+        {
+            m11 = matrix3.m11; m12 = matrix3.m12; m13 = matrix3.m13;
+            m21 = matrix3.m11; m22 = matrix3.m22; m23 = matrix3.m23;
+            m31 = matrix3.m31; m32 = matrix3.m32; m33 = matrix3.m33;
+        }
+        public void Set(float m11, float m12, float m13, float m21, float m22, float m23, float m31, float m32, float m33)
+        {
+            this.m11 = m11; this.m12 = m12; this.m13 = m13;
+            this.m21 = m21; this.m22 = m22; this.m23 = m23;
+            this.m31 = m31; this.m32 = m32; this.m33 = m33;
+        }
+        public void Set(Matrix3 matrix3)
+        {
+            m11 = matrix3.m11; m12 = matrix3.m12; m13 = matrix3.m13;
+            m21 = matrix3.m11; m22 = matrix3.m22; m23 = matrix3.m23;
+            m31 = matrix3.m31; m32 = matrix3.m32; m33 = matrix3.m33;
+        }
         public static Matrix3 operator *(Matrix3 lhs, Matrix3 rhs)
         {
             return new Matrix3(
@@ -41,6 +59,81 @@ namespace GameFramework
                 lhs.m11 * rhs.x + lhs.m12 * rhs.y + lhs.m13 * rhs.z,
                 lhs.m21 * rhs.x + lhs.m22 * rhs.y + lhs.m23 * rhs.z,
                 lhs.m31 * rhs.x + lhs.m32 * rhs.y + lhs.m33 * rhs.z);
+        }
+        public void SetScaled(float x, float y, float z)
+        {
+            m11 = x; m12 = 0; m13 = 0;
+            m21 = 0; m22 = y; m23 = 0;
+            m31 = 0; m32 = 0; m33 = z;
+        }
+        public void SetScaled(Vector3 v)
+        {
+            m11 = v.x; m12 = 0; m13 = 0;
+            m21 = 0; m22 = v.y; m23 = 0;
+            m31 = 0; m32 = 0; m33 = v.z;
+        }
+        public void Scale(float x, float y, float z)
+        {
+            Matrix3 m = new Matrix3();
+            m.SetScaled(x, y, z);
+
+            Set(this * m);
+        }
+        void Scale(Vector3 v)
+        {
+            Matrix3 m = new Matrix3();
+            m.SetScaled(v.x, v.y, v.z);
+            Set(this * m);
+        }
+        public void SetRotateX(double radians)
+        {
+            Set(1, 0, 0,
+                0, (float)Math.Cos(radians), (float)Math.Sin(radians),
+                0, (float)-Math.Sin(radians), (float)Math.Cos(radians));
+        }
+        public void RotateX(double radians)
+        {
+            Matrix3 m = new Matrix3();
+            m.SetRotateX(radians);
+
+            Set(this * m);
+        }
+        public void SetRotateY(double radians)
+        {
+            Set((float)Math.Cos(radians), 0, (float)-Math.Sin(radians),
+                0, 1, 0,
+                (float)Math.Sin(radians), 0, (float)Math.Cos(radians));
+        }
+        public void RotateY(double radians)
+        {
+            Matrix3 m = new Matrix3();
+            m.SetRotateX(radians);
+
+            Set(this * m);
+        }
+        public void SetRotateZ(double radians)
+        {
+            Set((float)Math.Cos(radians), (float)Math.Sin(radians), 0,
+                (float)-Math.Sin(radians), 1, (float)Math.Cos(radians),
+                0, 0, 1);
+        }
+        public void RotateZ(double radians)
+        {
+            Matrix3 m = new Matrix3();
+            m.SetRotateX(radians);
+
+            Set(this * m);
+        }
+        public void SetEuler(float pitch, float yaw, float roll)
+        {
+            Matrix3 x = new Matrix3();
+            Matrix3 y = new Matrix3();
+            Matrix3 z = new Matrix3();
+            x.SetRotateX(pitch);
+            y.SetRotateY(yaw);
+            z.SetRotateZ(roll);
+
+            Set(z * y * x);
         }
     }
 }
---
+++ GameFramework/Vector4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFramework
+{
+    class Vector4
+    {
+        public float x, y, z, w;
+
+        public Vector4()
+        {
+            x = 0;
+            y = 0;
+            z = 0;
+            w = 0;
+        }
+    }
+}
