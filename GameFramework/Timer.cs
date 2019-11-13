712d2117e7e4d65776cb5bf627f1fbd51dfe8009
Ryan LaCombe <ryanalacombe@gmail.com>
2019-11-13 13:32:17 -0600

    Added a timer

---
+++ GameFramework/Timer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace GameFramework
+{
+    class Timer
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        private long _currentTime = 0;
+        private long _previousTime = 0;
+        private float _deltaTime = 0.005f;
+
+        public Timer()
+        {
+            stopwatch.Start();
+        }
+        public void Restart()
+        {
+            stopwatch.Restart();
+        }
+        public float Seconds
+        {
+            get
+            {
+                return stopwatch.ElapsedMilliseconds / 1000.0f;
+            }
+        }
+        public float GetDeltaTime()
+        {
+            _previousTime = _currentTime;
+            _currentTime = stopwatch.ElapsedMilliseconds;
+            _deltaTime = (_currentTime - _previousTime) / 1000.0f;
+            return _deltaTime;
+        }
+    }
+}
